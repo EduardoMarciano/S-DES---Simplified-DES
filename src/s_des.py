77924fa5c09972bc13cb7eb450e7cee332267f59
Eduardo Marciano <89046882+EduardoMarciano@users.noreply.github.com>
2024-12-17 19:55:59 -0300

    Feature: Key generation

---
+++ src/s_des.py
@@ -0,0 +1,46 @@
+def P_10(key):
+    """
+    Performs the P10 permutation on a 10-bit key.
+    """
+    return [key[2], key[4], key[1], key[6], key[3], key[9], key[0], key[8], key[7], key[5]]
+
+def circular_left_shift(key):
+    """
+    Performs a circular left shift (LS-1) on both halves of a 10-bit key.
+    """
+    left  = key[:len(key)//2]
+    right = key[len(key)//2:]
+
+    left = left[1:] + left[:1]
+    right = right[1:] + right[:1]
+
+    return left+right
+
+def P_8(key):
+    """
+    Performs the P8 permutation on a 10-bit key.
+    """
+    return [key[5], key[2], key[6], key[3], key[7], key[4], key[9], key[8]]
+
+def s_des_generation_keys(key):
+    """
+    Generates the K1 and K2 keys for the S-DES encryption algorithm.
+    """
+    key_P10 = P_10(key)
+    key_P10 = circular_left_shift(key_P10)
+    k1 = P_8(key_P10)
+
+    key_P10 = circular_left_shift(key_P10)
+    key_P10 = circular_left_shift(key_P10)
+    k2 = P_8(key_P10)
+
+    return k1, k2
+
+# Original 10-bit key
+key = [1,0,1,0,0,0,0,0,1,0]
+
+# The two subkeys generated
+k1, k2 = s_des_generation_keys(key)
+
+print(f'Chave K1: {k1}')
+print(f'Chave K2: {k2}')
